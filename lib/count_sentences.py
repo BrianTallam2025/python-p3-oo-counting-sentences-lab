d99320353d2d532ce7b1728ec7ff913c8ac53055
Brian Tallam <tallambrian633@gmail.com>
2025-05-14 14:23:52 +0300

    count sent

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        end_marks = [".", "?", "!"]
+        count = 0
+        prev_char = ""
+        for char in self._value:
+            if char in end_marks and prev_char not in end_marks:
+                count += 1
+            prev_char = char
+        return count
